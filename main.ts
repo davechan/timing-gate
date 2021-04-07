f3e319e9e762469c3acfd0d11fd02f81123ed665
Dave Chan <davechan@users.noreply.github.com>
2021-04-07 14:13:19 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+let time = 0
 let t2 = 0
 let t1 = 0
 let read = 0
@@ -7,10 +8,17 @@ basic.forever(function () {
         if (read == 0) {
             t1 = input.runningTime()
             read = 1
+            basic.showIcon(IconNames.LeftTriangle)
         } else {
             t2 = input.runningTime()
-            basic.showNumber((t2 - t1) / 1000)
+            time = (t2 - t1) / 1000
             read = 0
+            sensors.setIntensity(7)
+            sensors.digit(Math.trunc(time) / 10, 0)
+            sensors.digit(Math.trunc(time) % 10, 1)
+            sensors.showDpAt(ledon_off._on, 1)
+            sensors.digit(Math.trunc(time * 100) % 100 / 10, 2)
+            sensors.digit(Math.trunc(time * 100) % 10, 3)
         }
     }
 })
